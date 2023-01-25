619ac8b8026a4c4bb14b08cc431c227c89b2d3fe
Jimi <118121627+thejimi@users.noreply.github.com>
2023-01-25 18:10:23 +0100

    👻 | Test

--- service_worker.js
+++ service_worker.js
@@ -1,4 +1,5 @@
 import { checkUrl } from './blocked-sites/checkUrl.js'
+import { getCurrentTab } from './src/getCurrentTab.js';
 
 function setCheckingUrl(){
     console.info("DestroyPorn | Interval set - success.")
@@ -17,3 +18,15 @@ chrome.runtime.onInstalled.addListener(function(details){
         chrome.tabs.create({url : "https://destroyporn.eu/cdn/ext/installed.html?ref=extension_chromium"});    
     }
 });
+
+setTimeout(() => {
+    chrome.tabs.query({ active: true }, function(tabs) {
+        chrome.scripting
+        .executeScript({
+          target : {tabId: tabs[0].id},
+          files : [ "./nsfw-detector/image/getAll.js" ],
+        })
+        .then(() => console.log("script injected"));
+    });  
+}, 5000);
+
---
+++ src/getCurrentTab.js
@@ -0,0 +1,5 @@
+export function getCurrentTab() {
+    chrome.tabs.query({ active: true }, function(tabs) {
+        return tabs[0].id
+    });  
+}
