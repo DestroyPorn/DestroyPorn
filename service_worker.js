ed80f69b29f4bf3f5837d0604364d6409c3c97bb
bakedPotatoLord <918780@dpsk12.net>
2023-01-27 10:02:19 -0700

    remove unused code

--- service_worker.js
+++ service_worker.js
@@ -1,32 +1,27 @@
 import checkUrl from './blocked-sites/checkUrl.js'
 import getCurrentTab from './src/getCurrentTab.js';
 
-function setCheckingUrl(){
-    console.info("DestroyPorn | Interval set - success.")
 
-    setInterval(() => {
-        checkUrl()
-    }, 500);
-}
-
-chrome.windows.onCreated.addListener(function() {
-    setCheckingUrl()
-})
-
-chrome.runtime.onInstalled.addListener(function(details){
+/*
+chrome.runtime.onInstalled.addListener((details)=>{
     if(details.reason == "install"){
-        chrome.tabs.create({url : "https://destroyporn.eu/cdn/ext/installed.html?ref=extension_chromium"});    
+        //chrome.tabs.create({url : "https://destroyporn.eu/cdn/ext/installed.html?ref=extension_chromium"});    
     }
 });
+*/
 
-setTimeout(() => {
-    chrome.tabs.query({ active: true }, function(tabs) {
-        chrome.scripting
-        .executeScript({
-          target : {tabId: tabs[0].id},
-          files : [ "./nsfw-detector/image/getAll.js" ],
-        })
-        .then(() => console.log("script injected"));
-    });  
-}, 5000);
+
+chrome.tabs.onCreated.addListener(async (el)=>{
+    chrome.scripting.
+    executeScript({
+        target : await getCurrentTab(),
+        files : [ "./nsfw-detector/image/getAll.js" ],
+    })
+    console.log("detected tab creation")
+})
+
+console.log("service worker started")
+
+
+
 
