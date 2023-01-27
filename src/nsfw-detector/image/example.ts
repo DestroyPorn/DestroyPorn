ee750fc8c80b6d3a7422fdea771cd1c8771201bf
bakedPotatoLord <918780@dpsk12.net>
2023-01-27 13:40:53 -0700

    switch to let and esm imports

--- src/nsfw-detector/image/example.ts
+++ src/nsfw-detector/image/example.ts
@@ -1,8 +1,9 @@
-export default async function example(){
-    const classify = require('./classify.js')
+import classify from './classify.js'
 
-    var nsfw_image_example = 'https://cdn.discordapp.com/attachments/1065928419059179522/1066087850371711047/meaAaGwObaaaamhsUPZEqiIbxKg8tny13.png' //example porn image
-    var results = await classify(nsfw_image_example, true)
+export default async function example(){
+
+    let nsfw_image_example = 'https://cdn.discordapp.com/attachments/1065928419059179522/1066087850371711047/meaAaGwObaaaamhsUPZEqiIbxKg8tny13.png' //example porn image
+    let results = await classify(nsfw_image_example)
     
     if(results.ClassifiedAs === 'NSFW'){
         return console.log(`This image has been classified as NSFW, the probability of it being a ${results.WinnerTag} image is ${results.Probability}.`)
