ed1de8b9b47f8807d2dbbe30cf04d9185df8a34f
bakedPotatoLord <918780@dpsk12.net>
2023-01-27 12:21:01 -0700

    clean up code

--- src/service_worker.ts
+++ src/service_worker.ts
@@ -1,19 +1,14 @@
 import getAll from "./nsfw-detector/image/getAll.js";
 
-
-
-
 chrome.runtime.onInstalled.addListener((details)=>{
     if(details.reason == "install"){
-        //chrome.tabs.create({url : "https://destroyporn.eu/cdn/ext/installed.html?ref=extension_chromium"});    
+        chrome.tabs.create({url : "https://destroyporn.eu/cdn/ext/installed.html?ref=extension_chromium"});    
     }
 });
-
 
 async function getCurrentTabId():Promise<number| undefined> {
     try{
         let queryOptions = { active: true, lastFocusedWindow: true };
-    
         let [tab] = await chrome.tabs.query(queryOptions);
         return tab.id;
     }catch(err){
@@ -21,20 +16,14 @@ async function getCurrentTabId():Promise<number| undefined> {
     }
 }
 
-
-
-
-chrome.tabs.onCreated.addListener(async (el)=>{
+chrome.tabs.onUpdated.addListener(async (tabId, info)=>{
     chrome.scripting.
     executeScript({
         target :{tabId: await getCurrentTabId()},
         func: getAll
     })
-    console.log("detected tab creation")
+    
+    console.log("detected tab update")
 })
 
 console.log("service worker started")
-
-
-
-
