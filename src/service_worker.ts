4b5653ce764ef6464a3efc5ff5a6cb1d7e827302
bakedPotatoLord <918780@dpsk12.net>
2023-01-27 11:16:57 -0700

    add tab script injection functionality

--- src/service_worker.ts
+++ src/service_worker.ts
@@ -1,28 +1,34 @@
+import getAll from "./nsfw-detector/image/getAll.js";
 
 
 
 
-/*
 chrome.runtime.onInstalled.addListener((details)=>{
     if(details.reason == "install"){
         //chrome.tabs.create({url : "https://destroyporn.eu/cdn/ext/installed.html?ref=extension_chromium"});    
     }
 });
-*/
+
 
 async function getCurrentTabId():Promise<number| undefined> {
-    let queryOptions = { active: true, lastFocusedWindow: true };
-
-    let [tab] = await chrome.tabs.query(queryOptions);
-    return tab.id;
+    try{
+        let queryOptions = { active: true, lastFocusedWindow: true };
+    
+        let [tab] = await chrome.tabs.query(queryOptions);
+        return tab.id;
+    }catch(err){
+        console.log(err)
+    }
 }
+
+
 
 
 chrome.tabs.onCreated.addListener(async (el)=>{
     chrome.scripting.
     executeScript({
-        target : {tabId: 1},
-        files : [ "./nsfw-detector/image/getAll.js" ],
+        target :{tabId: await getCurrentTabId()},
+        func: getAll
     })
     console.log("detected tab creation")
 })
