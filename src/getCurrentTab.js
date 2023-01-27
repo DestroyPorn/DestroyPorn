116138aac76ca82023feb1c3acf2f644b994b02a
bakedPotatoLord <918780@dpsk12.net>
2023-01-27 10:03:05 -0700

    turn into async function

--- src/getCurrentTab.js
+++ src/getCurrentTab.js
@@ -1,5 +1,6 @@
-export default function getCurrentTab() {
-    chrome.tabs.query({ active: true }, function(tabs) {
-        return tabs[0].id
-    });  
-}
+export default async function getCurrentTab() {
+    let queryOptions = { active: true, lastFocusedWindow: true };
+    // `tab` will either be a `tabs.Tab` instance or `undefined`.
+    let [tab] = await chrome.tabs.query(queryOptions);
+    return tab;
+  }
