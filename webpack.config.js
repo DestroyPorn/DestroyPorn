74a9fdce2d149a9f2d6a9df24faa652b8e379145
= <918780@dpsk12.net>
2023-01-28 07:04:48 -0700

    create webpack config

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+
+import path from "node:path"
+import webpack from "webpack"
+
+import * as url from 'url';
+const __filename = url.fileURLToPath(import.meta.url);
+const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
+
+export default {
+  entry: './src/service_worker.ts',
+  devtool: 'inline-source-map',
+  mode: "development",
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+}
+
+
