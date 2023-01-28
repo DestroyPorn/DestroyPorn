d8e4b683f27d0fff63102fd3f10f5f8004371bc6
bakedPotatoLord <918780@dpsk12.net>
2023-01-28 14:45:15 -0700

    simplify code

--- src/blocked-sites/checkUrl.ts
+++ src/blocked-sites/checkUrl.ts
@@ -1,26 +1,15 @@
-import { createBlacklist } from "../service_worker.js";
+import { createBlacklist, getCurrentTabId } from "../service_worker";
 
-export default function checkUrl(url:string | URL) {
+let blacklist = await createBlacklist()
 
-    async function fetchAndCheck(domainName){
-        let blacklist = await createBlacklist()
+export function checkUrl(url:string | URL) {
+    url = new URL(url)
+    return blacklist.has(url.hostname)
+}
 
-        var domain = domainName.toString().split('.').reverse().splice(0,2).reverse().join('.')
-
-        if(blacklist.has(domain)){
-            chrome.tabs.query({ active: true }, function(tabs) {
-                chrome.tabs.remove(tabs[0].id);
-                chrome.tabs.create({url : `https://destroyporn.eu/cdn/ext/blocked-website.html?ref=extension_chromium&website=${domain}`});     
-            });  
-
-            return;
-        } else {
-            
-        }
-        
-    }
-
-    function callbackFromClose(){
-        console.info("DestroyPorn | This tab contained adult content.")
-    }
+export async function replacecurrentTab(blockedSite:string){
+    await getCurrentTabId()
+    chrome.tabs.update({
+        url:`https://destroyporn.eu/cdn/ext/blocked-website.html?ref=extension_chromium&website=${blockedSite}`
+    })
 }
