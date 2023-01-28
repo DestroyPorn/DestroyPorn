47d7959399ece12ee8d303766d17e71cf69741e2
= <918780@dpsk12.net>
2023-01-28 07:02:41 -0700

    confert image string to image url

--- src/nsfw-detector/image/classify.ts
+++ src/nsfw-detector/image/classify.ts
@@ -1,9 +1,9 @@
-import getPredictions from "./getPredictions.js";
-
-
+import getPredictions from "./getPredictions";
 
 export default async function classify(imageURL:string) {
-    let predictions = await getPredictions(imageURL)
+    let im = new Image()
+    im.src = imageURL
+    let predictions = await getPredictions(im)
 
 
     if(predictions[0].className === 'Neutral'){
