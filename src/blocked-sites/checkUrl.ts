2290fa7389363bda0e1de1a71d096481a273a19a
= <918780@dpsk12.net>
2023-01-28 07:06:39 -0700

    move it all back one tab

--- src/blocked-sites/checkUrl.ts
+++ src/blocked-sites/checkUrl.ts
@@ -5,18 +5,18 @@ export default function checkUrl(url:string | URL) {
     async function fetchAndCheck(domainName){
         let blacklist = await createBlacklist()
 
-            var domain = domainName.toString().split('.').reverse().splice(0,2).reverse().join('.')
+        var domain = domainName.toString().split('.').reverse().splice(0,2).reverse().join('.')
 
-            if(blacklist.has(domain)){
-                chrome.tabs.query({ active: true }, function(tabs) {
-                    chrome.tabs.remove(tabs[0].id);
-                    chrome.tabs.create({url : `https://destroyporn.eu/cdn/ext/blocked-website.html?ref=extension_chromium&website=${domain}`});     
-                });  
+        if(blacklist.has(domain)){
+            chrome.tabs.query({ active: true }, function(tabs) {
+                chrome.tabs.remove(tabs[0].id);
+                chrome.tabs.create({url : `https://destroyporn.eu/cdn/ext/blocked-website.html?ref=extension_chromium&website=${domain}`});     
+            });  
 
-                return;
-            } else {
-                
-            }
+            return;
+        } else {
+            
+        }
         
     }
 
