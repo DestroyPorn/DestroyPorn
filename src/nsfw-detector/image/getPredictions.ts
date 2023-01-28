c770db7d34f39b0b4196a5e07d8647a435394d07
= <918780@dpsk12.net>
2023-01-28 07:07:04 -0700

    remove unused imports

--- src/nsfw-detector/image/getPredictions.ts
+++ src/nsfw-detector/image/getPredictions.ts
@@ -1,18 +1,8 @@
-import axios from "axios"
 import nsfw from "nsfwjs"
-import tf  from '@tensorflow/tfjs-node'
 
-
-export default async function getPredictions(imageURL:string) {
-    const pic = await axios.get(`${imageURL}`, {
-        responseType: 'arraybuffer',
-    })
+export default async function getPredictions(image:HTMLImageElement) {
     const model = await nsfw.load()
-      
-    const image = tf.node.decodeImage(pic.data,3,"int32",false)
-    //@ts-ignore
     const predictions = await model.classify(image)
-    image.dispose() 
     
     return predictions
 }
