7576e7fd312670e473f7327954ea3bf8da2af49d
bakedPotatoLord <918780@dpsk12.net>
2023-01-28 22:04:23 -0700

    comment out broken crap

--- src/injectorScript.ts
+++ src/injectorScript.ts
@@ -3,13 +3,13 @@ import nsfw from "@bakedpotatolord/nsfwjs"
 
 export async function injectorScript(){
   console.log("script ingected");
-  const model = await nsfw.load()
+  //const model = await nsfw.load()
 
   Array.from(document.getElementsByTagName('img'))
   .forEach(async (im)=>{
     console.log(im)
-    let [predictions] = await model.classify(im)
-    console.log(predictions)
+    //let [predictions] = await model.classify(im)
+    //console.log(predictions)
   })
 
 }
