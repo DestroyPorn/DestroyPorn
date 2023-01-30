4d70a74b842cec7a9934a9a2534c9cc002bd1f80
bakedPotatoLord <918780@dpsk12.net>
2023-01-29 18:11:14 -0700

    comment out nsfw functionality

--- src/injectorScript.ts
+++ src/injectorScript.ts
@@ -1,15 +1,15 @@
 
-import nsfw from "@bakedpotatolord/nsfwjs"
+//import nsfw from "@bakedpotatolord/nsfwjs"
 
 export async function injectorScript(){
-  console.log("script ingected");
-  const model = await nsfw.load()
+  console.log("script injected");
+  //const model = await nsfw.load()
 
   Array.from(document.getElementsByTagName('img'))
   .forEach(async (im)=>{
     console.log(im)
-    let [predictions] = await model.classify(im)
-    console.log(predictions)
+    //let [predictions] = await model.classify(im)
+    //console.log(predictions)
   })
 
 }
