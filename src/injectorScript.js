aba5530c3234a090ab764fbcfd623e2822b1469c
Jimi <118121627+thejimi@users.noreply.github.com>
2023-02-12 14:47:39 +0100

    Alpha v2.0.2

--- src/injectorScript.js
+++ src/injectorScript.js
@@ -9,6 +9,11 @@ export async function injectorScript(){
     var element = document.getElementById("destroyporn-running1")
     element.style.display = 'block'
   }
+
+  if(window.location.href.includes('https://destroyporn.eu/download')){
+    var element = document.getElementById("destroyporn-running1")
+    element.style.display = 'block'
+  }
 }
 
 injectorScript()
